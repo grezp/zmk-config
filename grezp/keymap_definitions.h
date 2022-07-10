26611bfbff401c74e59da07b56fdddae36155f96
grezp <grezp@pm.me>
2022-07-10 11:52:52 -0700

    Starting to organize files

---
+++ grezp/keymap_definitions.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define DEFAULT   0
+#define ALT_MOD   1
+#define FUN_LAY   2
+#define NUM_LAY   3
+#define SYM_LAY   4
+#define NAV_LAY   5
+
+#define LAYER_BASE_ALPHAS \
+&lt FUN_LAY TILDE  &kp Q              &kp W              &kp F              &kp P              &kp B              &kp J              &kp L              &kp U              &kp Y              &kp SEMI           &kp BSPC           \
+&lt NAV_LAY TAB    &kp A              &kp R              &kp S              &kp T              &kp G              &kp M              &kp N              &kp E              &kp I              &kp O              &kp SQT            \
+&mo ALT_MOD        &kp Z              &kp X              &kp C              &kp D              &kp V              &kp K              &kp H              &kp COMMA          &kp DOT            &kp FSLH           &kp DEL            \
+                                                         &kp LGUI           &lt SYM_LAY ESC    &btm LCTRL SPACE   &kp LSHFT          &lt NUM_LAY RET    &kp LALT
+
